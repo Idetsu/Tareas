8537c3c7944105ca93a25f320449c14ec6c587e0
Idetsu <128940398+Idetsu@users.noreply.github.com>
2024-04-30 10:35:39 -0400

    Listas.py

--- Listas.py
+++ Listas.py
@@ -5,14 +5,27 @@ import random as rd #as para cambiar el nombre de la libreria, en este caso ahor
 lista_1=[] #lista vacia
 lista_2=[5,"Juan",18.5,-8,True,"Lopez"] #lista con datos
 
-print(lista_2[4]) #Una lista empieza de 0, por tanto no se muestra -8 (posición 3), sino True (posición 4)
+print(lista_2[4]) #Una lista empieza de 0, por tanto no se muestra -8 (posicion 3), sino True (posicion 4)
 print(lista_2,("Vista developer")) # Vista developer
 
 for w in (lista_2): #Vista usuario
     print(w,end=" ") #Mostrar datos hacia al lado, el " " es para separar
 
-#lista_1.append(23)
-#print(lista_1)
+# OPERACIONES CON UNA LISTA 
+    #lista.append(dato)
+    #lista.insert(x,y)  // x=posicion  y=dato  //va hacia la posicion x e inserta el dato y
+    #Ejemplo, lista.insert(3,5), va hacia la posicion 3 e inserta el numero entero: 5
+
+    #lista.remove() //elimina un dato POR SU NOMBRE, NOTA: elimina solo 1 dato aunque este repetido, el primero que encuentre
+    #si se quiere eliminar mas de un dato se necesita usar un ciclo
+
+    #lista.pop() //Elimina el -1 si no tiene datos, elimina una POSICION
+    #lista.pop(3) //Elimina la posicion 3
+
+    #lista.count("Lopez") //Cuenta cuantos "Lopez" hay en la lista
+
+    #lista.sort(reverse=true) //Ordena de mayor a menor, pero al usar reverse queda de menor a mayor
+
 
 #llenar lista de numeros aleatorios:
 for z in range (10): #cuantos numeros
