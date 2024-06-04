6af90e38a7d52a83d39f39aa13f3d8829c37e833
Idetsu <128940398+Idetsu@users.noreply.github.com>
2024-06-04 10:44:52 -0400

    Update #Iniciar sesión.py

--- #Iniciar sesión.py
+++ #Iniciar sesión.py
@@ -6,22 +6,24 @@ z=0
 #Registrar con reglas de negocio
 #Por desarrollar Registro: Requerir mayuscula y al menos 1 numero
 def registrar():
-    user=0(input("Registre su nombre de usuario: "))
+    user=str(input("Registre su nombre de usuario: "))
     usr.append(user)
-    password=0(input("Registre su contraseña: "))
+    password=str(input("Registre su contraseña: "))
     passwd.append(password)
 #Por desarrollar Inicio: Crear usuarios independientes, por ahora, mientras ambos datos esten en la
 #lista, se puede iniciar sesión, lo que es incorrecto
 def iniciar():
-    userini=0(input("Ingrese su nombre de usuario: "))
-    passwordini=0(input("Ingrese su contraseña: "))
+    userini=str(input("Ingrese su nombre de usuario: "))
+    passwordini=str(input("Ingrese su contraseña: "))
     if userini in usr or passwordini in passwd:
         print("Se ha iniciado sesión.")
         ini=True
+        return ini
     else:
         print("Usuario o contraseña incorrectos.")
         ini=False
-#Menu no funciona, posible mala condicion del while
+        return ini
+
 while z!=3:
     print("1.- Registrarse")
     print("2.- Iniciar Sesión")
@@ -31,7 +33,7 @@ while z!=3:
     if z==1:
         registrar()
     if z==2:
-        iniciar()
+        ini=iniciar()
         if ini==True:
            while x!=4:
             print("1.- Comprar ...")
@@ -46,7 +48,8 @@ while z!=3:
             print("")
            if x==3:
             print("")
-        elif ini==False:
+        if ini==False:
            print("No se ha iniciado sesión.")
+           break
     if z==3:
         print("Saliendo de la tienda.")
